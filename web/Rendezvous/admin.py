c8d3d668a39f17889acb13a6b06d334f8dbc6dd3
siddharthaa <siddharthaa@iiitd.ac.in>
2011-05-17 09:21:14 +0000

    Added Books and papers

--- web/Rendezvous/admin.py
+++ web/Rendezvous/admin.py
@@ -1,8 +1,13 @@
 from iiitd.Rendezvous.models import Student, Course_registration, Responder, New_registration, Subject, Question, Response, Assignment, Call_detail
 from django.contrib import admin
 
+class Courses(admin.ModelAdmin):
+    list_display = ('studentID', 'subjectID')
+    list_filter = ('studentID', 'subjectID')
+
+
 admin.site.register(Student)
-admin.site.register(Course_registration)
+admin.site.register(Course_registration,Courses)
 admin.site.register(Responder)
 admin.site.register(New_registration)
 admin.site.register(Subject)
